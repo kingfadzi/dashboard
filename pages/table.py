5085b350d3dfa7f9a3e039c216b0a02fc2734fb5
kingfadzi <kingfadzi@gmail.com>
2025-02-08 12:53:53 -0500

    update table.py

--- pages/table.py
+++ pages/table.py
@@ -1,6 +1,7 @@
 import dash
 from dash import html, dash_table, dcc
 import dash_bootstrap_components as dbc
+from layouts.layout_filters import filter_layout
 
 dash.register_page(__name__, path="/table")
 
