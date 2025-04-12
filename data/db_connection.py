21cd1fd279900695fa55aacb0c665bbc2cc46507
kingfadzi <kingfadzi@gmail.com>
2025-04-11 21:31:37 -0400

    update db_connection.py

--- data/db_connection.py
+++ data/db_connection.py
@@ -1,5 +1,5 @@
 from sqlalchemy import create_engine
 
-DB_CONNECTION_STRING = "postgresql://postgres:postgres@192.168.1.188:5422/gitlab-usage"
+DB_CONNECTION_STRING = "postgresql://postgres:postgres@192.168.1.188:5432/gitlab-usage"
 
 engine = create_engine(DB_CONNECTION_STRING)
