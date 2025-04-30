7956c9787297812411aeb37b326d867d487d0539
kingfadzi <kingfadzi@gmail.com>
2025-04-30 18:13:23 -0400

    update giant_table.py

--- giant_table.py
+++ giant_table.py
@@ -42,4 +42,4 @@ def update_columns(tab_name):
     return [{"name": c, "id": c} for c in cols], df[cols].to_dict("records")
 
 if __name__ == '__main__':
-    app.run_server(debug=True)
+    app.run(debug=True)
