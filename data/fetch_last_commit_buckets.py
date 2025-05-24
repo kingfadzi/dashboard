2815c5a46833f0232e9fc352b735e86573011abf
kingfadzi <kingfadzi@gmail.com>
2025-05-24 09:39:33 -0400

    dymanic profiles

--- data/fetch_last_commit_buckets.py
+++ data/fetch_last_commit_buckets.py
@@ -2,7 +2,8 @@ from sqlalchemy import text
 import pandas as pd
 from data.cache_instance import cache
 from data.db_connection import engine
-from data.build_filter_conditions import build_filter_conditions
+from data.sql_filter_utils import build_repo_filter_conditions
+
 
 def fetch_last_commit_buckets(filters=None):
     @cache.memoize()
@@ -11,20 +12,22 @@ def fetch_last_commit_buckets(filters=None):
         SELECT * FROM (
             SELECT
                 CASE
-                    WHEN last_commit_date >= NOW() - INTERVAL '1 month' THEN '< 1 month'
-                    WHEN last_commit_date >= NOW() - INTERVAL '3 months' THEN '1-3 months'
-                    WHEN last_commit_date >= NOW() - INTERVAL '6 months' THEN '3-6 months'
-                    WHEN last_commit_date >= NOW() - INTERVAL '9 months' THEN '6-9 months'
-                    WHEN last_commit_date >= NOW() - INTERVAL '12 months' THEN '9-12 months'
-                    WHEN last_commit_date >= NOW() - INTERVAL '18 months' THEN '12-18 months'
-                    WHEN last_commit_date >= NOW() - INTERVAL '24 months' THEN '18-24 months'
+                    WHEN rm.last_commit_date >= NOW() - INTERVAL '1 month' THEN '< 1 month'
+                    WHEN rm.last_commit_date >= NOW() - INTERVAL '3 months' THEN '1-3 months'
+                    WHEN rm.last_commit_date >= NOW() - INTERVAL '6 months' THEN '3-6 months'
+                    WHEN rm.last_commit_date >= NOW() - INTERVAL '9 months' THEN '6-9 months'
+                    WHEN rm.last_commit_date >= NOW() - INTERVAL '12 months' THEN '9-12 months'
+                    WHEN rm.last_commit_date >= NOW() - INTERVAL '18 months' THEN '12-18 months'
+                    WHEN rm.last_commit_date >= NOW() - INTERVAL '24 months' THEN '18-24 months'
                     ELSE '24+ months'
                 END AS commit_bucket,
-                COUNT(DISTINCT repo_id) AS repo_count
-            FROM repo_metrics
+                COUNT(DISTINCT rm.repo_id) AS repo_count
+            FROM repo_metrics rm
+            INNER JOIN harvested_repositories hr ON rm.repo_id = hr.repo_id
+            WHERE 1=1
         """
         if condition_string:
-            sql += f" WHERE {condition_string}"
+            sql += f" AND {condition_string}"
         sql += """
             GROUP BY 1
         ) subquery
@@ -43,5 +46,5 @@ def fetch_last_commit_buckets(filters=None):
         stmt = text(sql)
         return pd.read_sql(stmt, engine, params=param_dict)
 
-    condition_string, param_dict = build_filter_conditions(filters)
+    condition_string, param_dict = build_repo_filter_conditions(filters)
     return query_data(condition_string, param_dict)
