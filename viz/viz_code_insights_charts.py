1d0da8976f7b9435d75aa7c47dc061713e4fe6de
kingfadzi <kingfadzi@gmail.com>
2025-05-31 15:42:19 -0400

    update viz_code_insights_charts.py

--- viz/viz_code_insights_charts.py
+++ viz/viz_code_insights_charts.py
@@ -1,18 +1,5 @@
-import plotly.express as px
-import pandas as pd
-from dash import dcc
-
-from viz.viz_code_insights_gitlog import (
-    render_avg_file_size_chart,
-    render_contributor_dominance_chart,
-    render_branch_sprawl_chart,
-    render_repo_age_chart,
-)
-
 def render_role_distribution_chart(df: pd.DataFrame):
-
     df["repo_count"] = pd.to_numeric(df["repo_count"], errors="coerce").fillna(0)
-
     top_langs = df.groupby("language")["repo_count"].sum().nlargest(20).index
     df_filtered = df[df["language"].isin(top_langs)]
 
@@ -22,9 +9,10 @@ def render_role_distribution_chart(df: pd.DataFrame):
         y="repo_count",
         color="language_role",
         barmode="stack",
-        #title="Language Role Distribution (Top 20)",
+        text="repo_count",        
     )
     fig.update_layout(
+        dragmode=False,          
         xaxis_title=None,
         title=None
     )
@@ -36,10 +24,11 @@ def render_normalized_weight_chart(df: pd.DataFrame):
         df,
         x="language",
         y="avg_percent_usage",
+        text="avg_percent_usage", 
     )
     fig.update_layout(
+        dragmode=False,
         xaxis_title=None,
         title=None
     )
     return dcc.Graph(id="normalized-weight-chart", figure=fig)
-
