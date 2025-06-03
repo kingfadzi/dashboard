d2489b108a181318db7abb88ead91293cdca48e9
kingfadzi <kingfadzi@gmail.com>
2025-06-03 15:38:46 -0400

    dymanic profiles

--- components/colors.py
+++ components/colors.py
@@ -1,12 +1,22 @@
 NEUTRAL_COLOR_SEQUENCE = [
-    "#4E79A7",
-    "#9F5FD3",
-    "#00A1C9",
-    "#F6C85F",
-    "#7A5195",
-    "#40BCD8",
-    "#FFB6C1",
-    "#6C4F86",
-    "#A2A8D3",
-    "#B8B8B8"
+    "#4E79A7",  # Blue
+    "#9F5FD3",  # Purple
+    "#00A1C9",  # Cyan
+    "#F6C85F",  # Yellow
+    "#7A5195",  # Dark Purple
+    "#40BCD8",  # Light Blue
+    "#FFB6C1",  # Pink
+    "#6C4F86",  # Indigo
+    "#A2A8D3",  # Pale Blue
+    "#B8B8B8",  # Gray
+    "#E15759",  # Red
+    "#76B7B2",  # Teal
+    "#59A14F",  # Green
+    "#EDC948",  # Mustard
+    "#AF7AA1",  # Lavender
+    "#FF9DA7",  # Light Red
+    "#9C755F",  # Brown
+    "#BAB0AC",  # Light Gray
+    "#D37295",  # Rose
+    "#FABFD2"   # Pale Pink
 ]
