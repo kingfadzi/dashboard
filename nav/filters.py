9be27975ace591371bcfb56b374a0fbd4bf8fa22
kingfadzi <kingfadzi@gmail.com>
2025-06-09 17:02:26 -0400

    update

---
+++ nav/filters.py
@@ -0,0 +1,57 @@
+import yaml
+from dash import dcc, html
+import dash_bootstrap_components as dbc
+
+# Load YAML once
+with open("filters.yaml") as f:
+    FILTERS = yaml.safe_load(f)["filters"]
+
+# List of all filter input IDs (including the app ID input)
+FILTER_IDS = list(FILTERS.keys()) + ["app-id-filter"]
+
+def filter_layout():
+    return html.Div([
+        dbc.Card(
+            dbc.CardBody(
+                dbc.Row(
+                    [
+                        dbc.Col(
+                            dcc.Dropdown(
+                                id=fid,
+                                options=conf.get("options", []),
+                                placeholder=conf.get("placeholder", ""),
+                                multi=True,
+                                clearable=True,
+                                style={
+                                    "fontSize": "14px",
+                                    "whiteSpace": "nowrap",
+                                    "overflow": "hidden",
+                                    "textOverflow": "ellipsis",
+                                },
+                                persistence=True,
+                                persistence_type="local",
+                            ), width=2
+                        )
+                        for fid, conf in FILTERS.items()
+                    ] + [
+                        dbc.Col(
+                            dcc.Input(
+                                id="app-id-filter",
+                                type="text",
+                                placeholder="Enter App ID or Repo Slug",
+                                debounce=True,
+                                className="form-control",
+                                style={"fontSize": "14px"},
+                                persistence=True,
+                                persistence_type="local",
+                            ), width=2
+                        )
+                    ],
+                    align="center",
+                    className="g-3",
+                )
+            ),
+            className="bg-light mb-4",
+        ),
+        html.Div(id="filter-debug", style={'border': '1px solid gray', 'padding': '10px'})
+    ])
---
+++ nav/nav.py
@@ -0,0 +1,40 @@
+from dash import Dash, dcc, html, Input, Output, page_container
+import dash_bootstrap_components as dbc
+from filters import filter_layout, FILTER_IDS
+
+app = Dash(
+    __name__,
+    use_pages=True,
+    suppress_callback_exceptions=True,
+    external_stylesheets=[dbc.themes.BOOTSTRAP]
+)
+
+app.layout = dbc.Container(
+    [
+        dcc.Location(id="url", refresh=False),
+        dcc.Location(id="repo-modal-location", refresh=False),
+        html.Div(id="repo-modal-container"),
+        dcc.Store(id="default-filter-store", storage_type="local"),
+        dbc.Collapse(filter_layout(), id="filter-collapse", is_open=True),
+        page_container,
+    ],
+    fluid=True,
+)
+
+@app.callback(
+    Output("default-filter-store", "data"),
+    [Input(fid, "value") for fid in FILTER_IDS],
+    prevent_initial_call=True,
+)
+def persist_filter_values(*values):
+    return dict(zip(FILTER_IDS, values))
+
+@app.callback(
+    Output("filter-debug", "children"),
+    Input("default-filter-store", "data"),
+)
+def debug(data):
+    return html.Pre(str(data))
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8050, debug=True)
